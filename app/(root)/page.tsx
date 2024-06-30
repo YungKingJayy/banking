b7e9e33a77840accf68a2a2da30cd5a864314e89
YungKingJayy <kalumcjethro@gmail.com>
2024-06-30 20:19:56 +0100

    finalize the app

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -48,7 +48,7 @@ const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
 
       <RIghtSidebar
         user={loggedIn}
-        transactions={accounts?.transactions}
+        transactions={account?.transactions}
         banks={accountsData?.slice(0, 2)}
       />
     </section>
--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -1,19 +1,20 @@
-import React from "react"
-import Link from "next/link"
-import { formatAmount } from "@/lib/utils"
+import React from "react";
+import Link from "next/link";
+import { formatAmount } from "@/lib/utils";
 import Image from "next/image"
+import Copy from "./Copy";;
 
 const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) => {
   return (
     <div className="flex flex-col">
       <Link
-        href="/"
+        href={`/transaction-history/?id=${account.appwriteItemId}`}
         className="bank-card"
       >
         <div className="bank-card_content">
           <div>
             <h1 className="text-16 font-semibold text-white">
-              {userName}
+              {account.name}
             </h1>
             <p className="font-ibm-plex-serif font-black text-white">
               {formatAmount(account.currentBalance)}
@@ -30,7 +31,7 @@ const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) =>
               </h2>
             </div>
               <p className="text-14 font-semibold tracking-[1.1px] text-white">
-                ●●●● ●●●● ●●●● <span className="text-16">1234</span>
+                ●●●● ●●●● ●●●● <span className="text-16">{account?.mask}</span>
               </p>
           </article>
         </div>
@@ -59,8 +60,10 @@ const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) =>
           className="absolute top-0 left-0"
         />
       </Link>
+
+      {showBalance && <Copy title= {account?.shareableId} />}
     </div>
   )
 }
 
-export default BankCard
+export default BankCard;
